8881f4cd98a9186fc7b7f712695d740964b28865
technikyvl <jakub4.biznes@gmail.com>
2025-10-26 20:01:28 +0100

    nowa apka

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-// Konfiguracja Vite dla aplikacji React + TypeScript
-export default defineConfig({
-  plugins: [react()],
-});
-
-
